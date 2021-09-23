f6e7952a033437ffb3812e926a3ca9be94aaf6c9
Steven <stevendkom@gmail.com>
2021-09-23 08:40:34 -0600

    Added the cdr function.

--- test/test_eval.py
+++ test/test_eval.py
@@ -2,16 +2,31 @@ from ltsp_test import *
 import random as rnd
 
 builtin_tests = [
+    # Car
     PexTest(
         "Eval car on simple list",
         [pair("(car (quote (a b c)))", "a")],
     ),
     PexTest(
-        "Eval car on nested list list",
+        "Eval car on nested list",
         [pair("(car (quote ((a b (x) c) f)))", "(a b (x) c)")],
     ),
     PexTest(
-        "Eval car on nested list list",
+        "Eval car on car of nested list",
         [pair("(car (car (quote ((y) b x c))))", "y")],
+    ),
+
+    # Cdr
+    PexTest(
+        "Eval cdr on simple list",
+        [pair("(cdr (quote (a b c)))", "(b c)")],
+    ),
+    PexTest(
+        "Eval cdr on nested list",
+        [pair("(cdr (quote ((a b (x) c) f)))", "(f)")],
+    ),
+    PexTest(
+        "Eval cdr on cdr of a list",
+        [pair("(cdr (cdr (quote (y x a b c))))", "(a b c)")],
     ),
 ]
