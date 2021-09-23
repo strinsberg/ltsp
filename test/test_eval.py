1de042bafd2ba83b97aca687f00ed59b8994c330
Steven <stevendkom@gmail.com>
2021-09-23 10:31:28 -0600

    Implemented cons in all the ways that I expect it should work currently. Also, updated pair printing to deal with lists with dotted pairs at the end. So we only print the dotted pair syntax when it is the only element, and also we don't print (a . NIL) for dotted pairs but just (a). There should probaly be more tests now that combine all three operations together. I think that the lisp 1.5 should have some identities that would be worth making into tests.

--- test/test_eval.py
+++ test/test_eval.py
@@ -15,10 +15,18 @@ builtin_tests = [
         "Eval car on car of nested list",
         [pair("(car (car (quote ((y) b x c))))", "y")],
     ),
+    PexTest(
+        "eval car on symbol",
+        [pair("(car a)", "NIL")],
+    ),
+    PexTest(
+        "eval cdr on empty list",
+        [pair("(car (quote ()))", "NIL")],
+    ),
 
     # Cdr
     PexTest(
-        "Eval cdr on simple list",
+        "eval cdr on simple list",
         [pair("(cdr (quote (a b c)))", "(b c)")],
     ),
     PexTest(
@@ -29,4 +37,47 @@ builtin_tests = [
         "Eval cdr on cdr of a list",
         [pair("(cdr (cdr (quote (y x a b c))))", "(a b c)")],
     ),
+    PexTest(
+        "eval cdr on symbol",
+        [pair("(cdr a)", "NIL")],
+    ),
+    PexTest(
+        "eval cdr on empty list",
+        [pair("(cdr (quote ()))", "NIL")],
+    ),
+
+    # Cons
+    PexTest(
+        "eval cons on a symbol and list",
+        [pair("(cons a (quote (b c)))", "(a b c)")],
+    ),
+    PexTest(
+        "eval cons on a list and list",
+        [pair("(cons (quote (a)) (quote (b c)))", "((a) b c)")],
+    ),
+    PexTest(
+        "eval cons on two symbols",
+        [pair("(cons a b)", "(a . b)")],
+    ),
+    PexTest(
+        "eval cons symbol onto cons list",
+        [pair("(cons a (cons b (quote (c d))))", "(a b c d)")],
+    ),
+    PexTest(
+        "eval cons symbol onto dotted pair two symbols",
+        [pair("(cons a (cons b c))", "(a b c)")],
+    ),
+    PexTest(
+        "eval cons on a symbol onto NIL",
+        [pair("(cons a NIL)", "(a)")],
+    ),
+    PexTest(
+        "eval cons on a symbol onto empty list",
+        [pair("(cons a (quote ()))", "(a)")],
+    ),
+    PexTest(
+        "eval cons on a symbol onto unquoted empty list",
+        [pair("(cons a ())", "(a)")],
+    ),
+
 ]
