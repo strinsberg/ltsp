8221b7acf5c75ad87078fa8d5ba36289e343fa4b
Steven <stevendkom@gmail.com>
2021-09-23 08:28:47 -0600

    Added the car function.

--- test/test_eval.py
+++ test/test_eval.py
@@ -4,6 +4,14 @@ import random as rnd
 builtin_tests = [
     PexTest(
         "Eval car on simple list",
-        [pair("(car (a b c))", "a")],
+        [pair("(car (quote (a b c)))", "a")],
+    ),
+    PexTest(
+        "Eval car on nested list list",
+        [pair("(car (quote ((a b (x) c) f)))", "(a b (x) c)")],
+    ),
+    PexTest(
+        "Eval car on nested list list",
+        [pair("(car (car (quote ((y) b x c))))", "y")],
     ),
 ]
