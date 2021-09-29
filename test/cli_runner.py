78abd976aeec5f4eb1c2dcca2fcd06df0e638b3b
Steven <stevendkom@gmail.com>
2021-09-29 11:21:28 -0600

    Moved eq? to core.

--- test/cli_runner.py
+++ test/cli_runner.py
@@ -8,7 +8,7 @@ test_suites = [
     #lt.LtspSuite("Reading Symbols", read.symbol_tests),
     #lt.LtspSuite("Reading Lists", read.list_tests),
     #lt.LtspSuite("Reading Numbers", read.number_tests),
-    lt.LtspSuite("Eval Builtin Functions", ev.builtin_tests),
+    lt.LtspSuite("Eval Elementary Functions", ev.builtin_tests),
     #lt.LtspSuite("Eval Special Forms", ev.special_form_tests),
     #lt.LtspSuite("Eval Symbols", ev.symbol_tests),
     #lt.LtspSuite("Core Artithmetic", core.arithmetic_tests),
